4edb0a6a8bc7b097a51a76641e2e01b477398ef9
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-06 16:16:29 +0200

    added auth service provider and controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+
+@Controller("auth")
+export class AuthController{
+
+    //here, we have injected the AuthService (this is called dependency injection)
+    
+    constructor(private authService: AuthService){
+      
+    }
+
+    //you add a method (post) with @Post decorator and the handler
+
+    // signup
+    @Post("signup")
+    signup(){
+
+    }
+
+    // signup
+
+    // login
+    @Post("signin")
+    signin(){
+
+    }
+
+    // login
+
+    
+}
